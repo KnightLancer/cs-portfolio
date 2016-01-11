f74e7897311003716ef0a7679e51a48244f851ce
knightlancer <lkyi.sh@mysummitps.org>
2016-01-11 03:23:46 +0000

    how can i make images appear with my text on click

--- infosite.js
+++ infosite.js
@@ -5,37 +5,27 @@ var item4 = document.getElementById("item4");
 var item5 = document.getElementById("item5");
 
 var info1 = document.getElementById("info1");
-var info2 = document.getElementById("info2");
 var info3 = document.getElementById("info3");
-var info4 = document.getElementById("info4");
 var info5 = document.getElementById("info5");
-var info6 = document.getElementById("info6");
 var info7 = document.getElementById("info7");
-var info8 = document.getElementById("info8");
 var info9 = document.getElementById("info9");
-var info10 = document.getElementById("info10");
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+    info1.innerHTML = "Brave Frontier is, by far, the most addicting game app on my Phone. It is a mobile Japanese role playing, turn-based RPG that has its own English copy in the US. It utilizes touch controls and the raising and battling of summoned characters called units. These units are used throughout the game by a summoner, with a name given by the player, as you travel the world of Grand Gaia and protect the lands from threats such as the Four Fallen Gods. The combat system is very basic yet challenging, which is the main reason why I enjoy this game. Harnessing the different types of units and their special attacks, or Brave Bursts - be it attacking, supporting, or healing - the summoner, as well as other allies, quest on. Aside from the main story, there are event dungeons and trials which, aside from being awarded goodies, bring about a sense of completion. The most annoying part of the game is having that taunting Rare Summon door always in front of you, tempting to deplete your Gems, you most valuable currency, to summon powerful units, most likely a copy of one you already have or one you don't need/want. I'm so glad it's free on the App Store.";
 });
                        
 item2.addEventListener("click", function(){
-    info3.innerHTML = "Item 2 stuff";
-    info4.innerHTML = "Item 2 other stuff";
+    info3.innerHTML = "Deemo is on my list because it falls under one my my favorite game genres: music. Deemo is a score-based, music and rhythm video game which uses many non-classical piano songs to progress in the plot or to have fun. Notes fall from the top of the screen according to the beat of the song - melody or harmony - and you tap them once they reach the bottom bolded line, which times aligns itself with the song, so you don't need to care about timing, but you need a good ear for music. I enjoy this game not only for its short but sweet story, but for its no-internet, smooth gameplay. Although I admit some songs aren't to everyone's taste, while other songs don't sound like it has a piano in it, the majority of the songs it has are worth the $2.";
 });
 
 item3.addEventListener("click", function(){
-   info5.innerHTML = "Item 3 stuff";
-   info6.innerHTML = "Item 3 more stuff";
+   info5.innerHTML = "Rune Story is a game I recently found and initially rejected, but came to be entertained by it. It's not you average action RPG on the App Store. I like this game based on its gameplay, whereas you can summon units using jewels like Brave Frontier, but when you go into battle, you are able to freely roam the map, defeating enemies with combos and skills. Everything is touched based and the battle depends on you, so every moment in battle needs your attention, lest you wish to be food for the monsters. This game is free, making it more enjoyable.";
 });
 
 item4.addEventListener("click", function(){
-    info7.innerHTML = "Item 4 stuff";
-    info8.innerHTML = "Item 4 more stuff";
-})
+    info7.innerHTML = "Osu is another rhythm game, minus the piano, but plus the action. Unlike Deemo, which has notes falling from the top, osu has beat circles which appear on screen with another circle which closes in on the beat circle in tune with the beat of the music. These beat circles, along with beat sliders and spinners are mapped out for each song, making up a beatmap. It gets hectic when faced with faster songs and wilder beatmaps, but stays true to its rhythm gaming. Plus, it's free!";
+});
 
 item5.addEventListener("click", function(){
-    info9.innerHTML = "Item 5 stuff";
-    info10.innerHTML = "Item 5 more stuff";
-})
+    info9.innerHTML = "By this time it was hard to choose a 5th game, since my choices were diminished, but from what I had left over, the 5th spot went to Danmaku Unlimited 2, a paid game with a very hellish game style. It falls under the genre of danmaku, or barrage in Japanese, and it's a fitting name. This game is what you would call a bullet-hell, as the game is more focused on weaving through intricate enemy bullet patterns and outnumbered enemy forces rather then shooting them down. The fast paced, never-ending danger gameplay brings that adrenaline rush when knowing that one false finger drag could take you down. I thought it was worth it for $4.99, because I never beat the last level, even on the easiest difficulty.";
+});
