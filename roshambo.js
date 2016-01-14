e3479f7371e90b5a38228e1f4f9ed36829f83402
knightlancer <lkyi.sh@mysummitps.org>
2016-01-14 05:29:27 +0000

    randomizing button positions

--- roshambo.js
+++ roshambo.js
@@ -1,3 +1,16 @@
-if (player1 == 'rock'){
-    
-}
+// JavaScript File
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+
+
+var positions = Array["105px","605px","1040px"];
+
+var Array = positions[Math.floor(Math.random() * positions.length)];
+
+paper.style.left = "150px";
+rock.style.left = "605px";
+scissors.style.left = "1040px";
+
+
+
