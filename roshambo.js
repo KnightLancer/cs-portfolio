4d2b8fbe548a41d229fe6820565378ca7887794f
knightlancer <lkyi.sh@mysummitps.org>
2016-01-14 23:04:24 +0000

    basically done

--- roshambo.js
+++ roshambo.js
@@ -4,13 +4,16 @@ var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
 
 
-var positions = Array["105px","605px","1040px"];
+var positions = ["105px","605px","1040px"];
 
-var Array = positions[Math.floor(Math.random() * positions.length)];
+var randposition1 = positions[Math.floor(Math.random() * positions.length)];
+var randposition2 = positions[Math.floor(Math.random() * positions.length)];
+var randposition3 = positions[Math.floor(Math.random() * positions.length)];
 
-paper.style.left = "150px";
-rock.style.left = "605px";
-scissors.style.left = "1040px";
+paper.style.left = randposition1;
+rock.style.left = randposition2;
+scissors.style.left = randposition3;
+
 
 
 
