bf71cd70611456a293add8f61c2ecd017a4fd90a
knightlancer <lkyi.sh@mysummitps.org>
2016-01-15 21:50:09 +0000

    done

--- roshambo.js
+++ roshambo.js
@@ -2,18 +2,17 @@
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
-
+// variables for the choices to connect HTML to JS
 
 var positions = ["105px","605px","1040px"];
+// I need the array in order for the randomizing to occur so the function below picks the values from above
 
 var randposition1 = positions[Math.floor(Math.random() * positions.length)];
 var randposition2 = positions[Math.floor(Math.random() * positions.length)];
 var randposition3 = positions[Math.floor(Math.random() * positions.length)];
+// the randomizing function, one for each choice
 
 paper.style.left = randposition1;
 rock.style.left = randposition2;
 scissors.style.left = randposition3;
-
-
-
-
+// connects the choices to the randomizing function
