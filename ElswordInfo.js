120f0b3a98be8485dd4cea74a1f13885f486455e
knightlancer <lkyi.sh@mysummitps.org>
2016-03-29 22:29:38 +0000

    more work. I got innerHTML down. need to figure out background change.

---
+++ ElswordInfo.js
@@ -0,0 +1,133 @@
+// JavaScript File
+var BaseElsword = document.getElementById("BaseElsword");
+var BaseAisha = document.getElementById("BaseAisha");
+var BaseRena = document.getElementById("BaseRena");
+var BaseRaven = document.getElementById("BaseRaven");
+var BaseEve = document.getElementById("BaseEve");
+var BaseChung = document.getElementById("BaseChung");
+var BaseAra = document.getElementById("BaseAra");
+var BaseElesis = document.getElementById("BaseElesis");
+var BaseAdd = document.getElementById("BaseAdd");
+var BaseLuciel = document.getElementById("BaseLuciel");
+
+BaseElsword.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+    
+    BaseElswordInfo.innerHTML="Hi"
+});
+
+BaseAisha.addEventListener("click", function() {
+    BaseElsword.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseRena.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseElsword.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseRaven.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseElsword.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseEve.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseElsword.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseChung.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseElsword.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseAra.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseElsword.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseElesis.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElsword.style.display="none"
+    BaseAdd.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseAdd.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseElsword.style.display="none"
+    BaseLuciel.style.display="none"
+});
+
+BaseLuciel.addEventListener("click", function() {
+    BaseAisha.style.display="none"
+    BaseRena.style.display="none"
+    BaseRaven.style.display="none"
+    BaseEve.style.display="none"
+    BaseChung.style.display="none"
+    BaseAra.style.display="none"
+    BaseElesis.style.display="none"
+    BaseAdd.style.display="none"
+    BaseElsword.style.display="none"
+});
