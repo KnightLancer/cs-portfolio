3c1231d889ac1482a82514c360e92255477e9453
knightlancer <lkyi.sh@mysummitps.org>
2016-03-30 02:23:22 +0000

    repeating wallpapersgit add -A! arrrggghhhh

--- ElswordInfo.js
+++ ElswordInfo.js
@@ -1,4 +1,8 @@
 // JavaScript File
+var firstimage = document.getElementById("firstimage");
+var firsttext = document.getElementById("firsttext");
+var firstinfo = document.getElementById("firstinfo");
+
 var BaseElsword = document.getElementById("BaseElsword");
 var BaseAisha = document.getElementById("BaseAisha");
 var BaseRena = document.getElementById("BaseRena");
@@ -10,124 +14,180 @@ var BaseElesis = document.getElementById("BaseElesis");
 var BaseAdd = document.getElementById("BaseAdd");
 var BaseLuciel = document.getElementById("BaseLuciel");
 
+var BaseElswordInfo = document.getElementById("BaseElswordInfo");
+
 BaseElsword.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
     
-    BaseElswordInfo.innerHTML="Hi"
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    BaseElswordInfo.innerHTML="Hey, I'm Elsword! I battle with my trusty sword!";
 });
 
 BaseAisha.addEventListener("click", function() {
-    BaseElsword.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseElsword.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
 });
 
 BaseRena.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseElsword.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
 });
 
 BaseRaven.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseElsword.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('http://elwiki.net/wiki/images/thumb/4/4f/4-5new.jpg/800px-4-5new.jpg')"
 });
 
 BaseEve.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseElsword.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('http://electronasod.weebly.com/uploads/2/6/9/5/26957661/2537015_orig.jpg')"
 });
 
 BaseChung.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseElsword.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('http://corporate.gameforge.com/wp-content/uploads/2013/01/city_of_hamel.jpg')"
 });
 
 BaseAra.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseElsword.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('http://www.mmogames.com/wp-content/uploads/2013/12/Elsword_Sander_2.jpg')"
 });
 
 BaseElesis.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElsword.style.display="none"
-    BaseAdd.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('http://elwiki.net/wiki/images/thumb/2/2b/VelderPlace.jpg/800px-VelderPlace.jpg')"
 });
 
 BaseAdd.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseElsword.style.display="none"
-    BaseLuciel.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseElsword.style.display = "none";
+    BaseLuciel.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('http://orig05.deviantart.net/aa10/f/2014/196/5/7/so_i_kept_pausing_the_video_in_add_s_anime_trailer_by_moonstar34-d7qsena.png')"
 });
 
 BaseLuciel.addEventListener("click", function() {
-    BaseAisha.style.display="none"
-    BaseRena.style.display="none"
-    BaseRaven.style.display="none"
-    BaseEve.style.display="none"
-    BaseChung.style.display="none"
-    BaseAra.style.display="none"
-    BaseElesis.style.display="none"
-    BaseAdd.style.display="none"
-    BaseElsword.style.display="none"
+    firstimage.style.display = "none";
+    firsttext.style.display = "none";
+    firstinfo.style.display = "none";
+    
+    BaseAisha.style.display = "none";
+    BaseRena.style.display = "none";
+    BaseRaven.style.display = "none";
+    BaseEve.style.display = "none";
+    BaseChung.style.display = "none";
+    BaseAra.style.display = "none";
+    BaseElesis.style.display = "none";
+    BaseAdd.style.display = "none";
+    BaseElsword.style.display = "none";
+    
+    document.body.style.backgroundImage = "url('https://secure.static.tumblr.com/543163caf2492801ced5400cc416a4f9/niofgx9/oOxnfbr7h/tumblr_static_d2xnjv4bdm0o888owww80sk00_640_v2.jpg')"
 });
