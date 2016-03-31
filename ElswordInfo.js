2eff05afdc1dcdbfe1dde7b32012ff78748425ca
knightlancer <lkyi.sh@mysummitps.org>
2016-03-31 03:49:13 +0000

    add character descriptions and, if I can improve outside expeditions, add character classes

--- ElswordInfo.js
+++ ElswordInfo.js
@@ -26,6 +26,17 @@ var BaseAddQuote = document.getElementById("BaseAddQuote");
 var BaseLucielQuote1 = document.getElementById("BaseLucielQuote1");
 var BaseLucielQuote2 = document.getElementById("BaseLucielQuote2");
 
+var BaseElswordInfo = document.getElementById("BaseElswordInfo");
+var BaseAishaInfo = document.getElementById("BaseAishaInfo");
+var BaseRenaInfo = document.getElementById("BaseRenaInfo");
+var BaseRavenInfo = document.getElementById("BaseRavenInfo");
+var BaseEveInfo = document.getElementById("BaseEveInfo");
+var BaseChungInfo = document.getElementById("BaseChungInfo");
+var BaseAraInfo = document.getElementById("BaseAraInfo");
+var BaseElesisInfo = document.getElementById("BaseElesisInfo");
+var BaseAddInfo = document.getElementById("BaseAddInfo");
+var BaseLucielInfo = document.getElementById("BaseLucielInfo");
+
 
 BaseElsword.addEventListener("click", function() {
     firstimage.style.display = "none";
@@ -43,6 +54,8 @@ BaseElsword.addEventListener("click", function() {
     BaseLuciel.style.display = "none";
     
     BaseElswordQuote.innerHTML = "'Hey, I'm Elsword! I battle with my trusty sword!'";
+    
+    BaseElswordInfo.innerHTML = "Elsword is that one hotheaded swordsman with confidence that overpowers his brains. Wielding a greatsword with only one hand, Elsword chops down any foe, big or small, as he and his allies search for his village's precious gem, the El Shard. He forms the 'El Search Party', but even as he finds the El, many more adventures beckon to his friends. As players, you can take control of this powerhouse, dishing out an array of heavy attacks, magical projectiles, and quick double-sword blows, depending on which path of the Knight you choose to follow.";
 });
 
 BaseAisha.addEventListener("click", function() {
@@ -61,6 +74,9 @@ BaseAisha.addEventListener("click", function() {
     BaseLuciel.style.display = "none";
     
     BaseAishaQuote.innerHTML = "'Prepare to be blown away by magic!'";
+    
+    BaseAishaInfo.innerHTML = "'This knowledgeable yet brash magician had her abilities stolen by an evil ring. Her mentor believes it may be linked to the El Shard, and so her adventure to regain her magic and recover the El begins. . . with her running into a certain hot-blooded knight. With her arsenal of magic attacks and supports, she can spam fire and summon ice, and when her enemies are close, a good smack of her staff is enough to put out their lights. Her magic capabilities allow her to perform several helpful actions, such as recharge power, float, and teleport. This mystical superpower will surely make enemies regret they ever crossed paths this this magician.'"
+    
 });
 
 BaseRena.addEventListener("click", function() {
@@ -79,6 +95,8 @@ BaseRena.addEventListener("click", function() {
     BaseLuciel.style.display = "none";
     
     BaseRenaQuote.innerHTML = "'Can you hear the spirit of the winds?'";
+    
+    BaseRenaInfo.innerHTML = "'An elven warrior who sets out to quell the disasters of the world. At the call of the stolen El Shard, Rena embarks with a swordsman and magician to chase after the El, sniping enemies one by one with her magic arrows. And when enemies get too close, she lashes out with devastating kicks. Calling upon the wind spirits, she can cast wind magic to blow opponents away and, with the help of the spirit of fire, can shoot a beautiful but deadly phoenix attack. By utilizing long and short ranged attacks, she is a force to be reckoned with.'"
 });
 
 BaseRaven.addEventListener("click", function() {
@@ -208,7 +226,7 @@ BaseAdd.addEventListener("click", function() {
     
     BaseAddQuote.innerHTML = "'Heheh.... the laws of this world... I shall decide them!'";
     
-    document.body.style.backgroundImage = "url('http://orig05.deviantart.net/aa10/f/2014/196/5/7/so_i_kept_pausing_the_video_in_add_s_anime_trailer_by_moonstar34-d7qsena.png')"
+    document.body.style.backgroundImage = "url('http://orig05.deviantart.net/aa10/f/2014/196/5/7/so_i_kept_pausing_the_video_in_add_s_anime_trailer_by_moonstar34-d7qsena.png')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundAttachment = "fixed";
 });
@@ -231,6 +249,8 @@ BaseLuciel.addEventListener("click", function() {
     BaseLucielQuote1.innerHTML = "'Status, honor, power. I've lost everything. Before the embers of my life go out... will you ... become my royal strength ... ?' - Lu";
     
     BaseLucielQuote2.innerHTML = "'I'll retake the throne ... I'll give my soul for you ... The contract is complete.' - Ciel";
+    
+    BaseLucielInfo.innerHTML = "A high ranking demon lord betrayed by those of her kind and forced to flee to another world. A famous assassin of the criminal underworld, grown up lonely and with his will steeled to survive, who's thrusted into the conflict with demons. That is who Luciela 'Lu' R. Sourcream and Ciel are. Bound together by a fateful encounter, this duo sets of with the rest of the El Search Party, eradicating demons and saving people all over the world. As a dual character, Lu and Ciel are able to switch out during attacks and skills, dealing quick and devastating damage to anyone in their way. By utilizing not just one of the chracters but both, players can execute demonic magic and swift punches alongside the many players of Elsword.";
     
     document.body.style.backgroundImage = "url('https://secure.static.tumblr.com/543163caf2492801ced5400cc416a4f9/niofgx9/oOxnfbr7h/tumblr_static_d2xnjv4bdm0o888owww80sk00_640_v2.jpg')";
     document.body.style.backgroundSize = "cover";
