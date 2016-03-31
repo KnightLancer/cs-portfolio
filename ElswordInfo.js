93ba4c5410a47d3b53e4871bb4830b1308113b3b
knightlancer <lkyi.sh@mysummitps.org>
2016-03-31 22:16:45 +0000

    basically done, but I can do much more

--- ElswordInfo.js
+++ ElswordInfo.js
@@ -55,7 +55,7 @@ BaseElsword.addEventListener("click", function() {
     
     BaseElswordQuote.innerHTML = "'Hey, I'm Elsword! I battle with my trusty sword!'";
     
-    BaseElswordInfo.innerHTML = "Elsword is that one hotheaded swordsman with confidence that overpowers his brains. Wielding a greatsword with only one hand, Elsword chops down any foe, big or small, as he and his allies search for his village's precious gem, the El Shard. He forms the 'El Search Party', but even as he finds the El, many more adventures beckon to his friends. As players, you can take control of this powerhouse, dishing out an array of heavy attacks, magical projectiles, and quick double-sword blows, depending on which path of the Knight you choose to follow.";
+    BaseElswordInfo.innerHTML = "Elsword is that one hotheaded swordsman with confidence that overpowers his brains. Wielding a greatsword with only one hand, Elsword chops down any foe, big or small, as he and his allies search for his village's precious gem, the El Shard, and retrieve it from the clutches of evil. All the while, Elsword searches for his sister, who went on to the Velder Kingdom to combat and even larger threat. Elsword forms the 'El Search Party', but even as he finds the El, many more adventures beckon to his friends. As players, you can take control of this powerhouse, dishing out an array of heavy attacks, magical projectiles, and quick double-sword blows, depending on which path of the Knight you choose to follow.";
 });
 
 BaseAisha.addEventListener("click", function() {
@@ -75,7 +75,7 @@ BaseAisha.addEventListener("click", function() {
     
     BaseAishaQuote.innerHTML = "'Prepare to be blown away by magic!'";
     
-    BaseAishaInfo.innerHTML = "'This knowledgeable yet brash magician had her abilities stolen by an evil ring. Her mentor believes it may be linked to the El Shard, and so her adventure to regain her magic and recover the El begins. . . with her running into a certain hot-blooded knight. With her arsenal of magic attacks and supports, she can spam fire and summon ice, and when her enemies are close, a good smack of her staff is enough to put out their lights. Her magic capabilities allow her to perform several helpful actions, such as recharge power, float, and teleport. This mystical superpower will surely make enemies regret they ever crossed paths this this magician.'"
+    BaseAishaInfo.innerHTML = "'This knowledgeable yet brash magician had her abilities stolen by an evil ring. Her mentor believes it may be linked to the El Shard, and so her adventure to regain her magic and recover the El begins. . . with her running into a certain hot-blooded knight. With her arsenal of magic attacks and supports, she can spam fire and summon ice, and when her enemies are close, a good smack of her staff is enough to put out their lights. Her magic capabilities allow her to perform several helpful actions, such as recharge power, float, and teleport. This mystical superpower will surely make enemies regret they ever crossed paths this this magician.'";
     
 });
 
@@ -96,7 +96,7 @@ BaseRena.addEventListener("click", function() {
     
     BaseRenaQuote.innerHTML = "'Can you hear the spirit of the winds?'";
     
-    BaseRenaInfo.innerHTML = "'An elven warrior who sets out to quell the disasters of the world. At the call of the stolen El Shard, Rena embarks with a swordsman and magician to chase after the El, sniping enemies one by one with her magic arrows. And when enemies get too close, she lashes out with devastating kicks. Calling upon the wind spirits, she can cast wind magic to blow opponents away and, with the help of the spirit of fire, can shoot a beautiful but deadly phoenix attack. By utilizing long and short ranged attacks, she is a force to be reckoned with.'"
+    BaseRenaInfo.innerHTML = "'An elven warrior who sets out to quell the disasters of the world. At the call of the stolen El Shard, Rena embarks with a swordsman and magician to chase after the El, sniping enemies one by one with her magic arrows. And when enemies get too close, she lashes out with devastating kicks. Calling upon the wind spirits, she can cast wind magic to blow opponents away and, with the help of the spirit of fire, can shoot a beautiful but deadly phoenix attack. By utilizing long and short ranged attacks, she is a force to be reckoned with. If looks could kill, she'd cause a calamity.";
 });
 
 BaseRaven.addEventListener("click", function() {
@@ -115,6 +115,8 @@ BaseRaven.addEventListener("click", function() {
     BaseLuciel.style.display = "none";
     
     BaseRavenQuote.innerHTML = "'Have I helped a little for the peace of this world...?'";
+    
+    BaseRavenInfo.innerHTML = "Raven is a man of honor and repentence who, in his days as the Captan of the Crow Merceneries of the Velder Kingdom, earned a high rank within society despite being a commoner. This caused jealously and anomosity among his fellow citizens, and was soon framed for treason. His comrades and fiancee rescued Raven and escaped to the forest, but were soon caught and killed, Raven being the last to suffer. As he was left for dead, a robot race called the Nasod approached him and offered revanged. The mercenery was later equipped with a Nasod arm and fought to destroy his kingdom, until he met a party of 3: a red-haired swordsman, a grape-haired magician, and an elven markswoman who highly resembled his dead fiancee. The group helped him realize his duty to protect his people. Thus, he joins the party to search for the El and atone for all the destruction he has caused. Players are able to experience a warrior who is not only quick with a blade, but he is adept in using his prototype Nasod arm to deliver menacing flame attacks and powerful blows. As he makes the trek to repentance, players can elimante the real evil that taints the lands.";
     
     document.body.style.backgroundImage = "url('http://elwiki.net/wiki/images/thumb/4/4f/4-5new.jpg/800px-4-5new.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -138,6 +140,8 @@ BaseEve.addEventListener("click", function() {
     
     BaseEveQuote.innerHTML = "'You will now face the queen of Nasods.'";
     
+    BaseEveInfo.innerHTML = "As the 'Little Queen of the Nasods', a profound race of intelligent robots, Eve held the responsibility of every bot's well being underneath her upper class rank in ancient times. That is, until the ancient humans and Nasods flew into a war with each other in response to a weakening El. The Nasods were denied help from the humans, and Eve was sent to her preservation capsule to avoid the fight. Thousands of years passed, and Eve was awoken by a stray El fragment. She awoke to discover her whole race had diminished. She aimed to revive the Nasods by first bringing back the King of Nasods, which forced her into another slumber. However, the King wished to continue the war between man and machine, and, using a certain stolen El, started mass-producing Nasods. It wasn't until a band of heroes led a red-haired swordsboy that the King was once again defeated, and Eve, who had awoken to find herself along again, decided to folow the party with her goal set to revive her race once more. Besides, this was the first time a boy asked to be her friend.";
+    
     document.body.style.backgroundImage = "url('http://electronasod.weebly.com/uploads/2/6/9/5/26957661/2537015_orig.jpg')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundAttachment = "fixed";
@@ -159,6 +163,8 @@ BaseChung.addEventListener("click", function() {
     BaseLuciel.style.display = "none";
     
     BaseChungQuote.innerHTML = "'I will ensure no one will get hurt!'"
+    
+    BaseChungInfo.innerHTML = "This little trap is not someone to be taken lightly. Chung, or formally 'Prince Seiker', the White Wolf of Hamel, is a prince of the royal Hamel City within the Senace Kingdom, which is most known for its majestic aquatic structures and features. All was peaceful in this bright blue city until it came under attack by demons. His father, the White Colassus Helputt, devoted his heart and soul to protecting his city, but was soon possessed by the demons and led them into his very own city. Chung's efforts to halt his father were futile, and he was gravely injured. Fortunately, a red swordsman and his group intervened and aided in his retreat. Chung vowed to one day return to his home and defend it like a true Guardian. Despite his seemingly small physique, he can wield a double-edged cannon with ease and comes fully equipped a special panoply he calls the Freiturnier. With this, he becomes a defensive player who can deliver devastating damage to any foe in his path."
     
     document.body.style.backgroundImage = "url('http://corporate.gameforge.com/wp-content/uploads/2013/01/city_of_hamel.jpg')"
     document.body.style.backgroundSize = "cover";
@@ -182,7 +188,9 @@ BaseAra.addEventListener("click", function() {
     
     BaseAraQuote.innerHTML = "'Until I face my brother again... I cannot give up!'";
     
-    document.body.style.backgroundImage = "url('http://www.mmogames.com/wp-content/uploads/2013/12/Elsword_Sander_2.jpg')"
+    BaseAraInfo.innerHTML = "Ara Haan was born in the town of Fahrmann"
+    
+    document.body.style.backgroundImage = "url('http://www.mmogames.com/wp-content/uploads/2013/12/Elsword_Sander_2.jpg')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundAttachment = "fixed";
 });
